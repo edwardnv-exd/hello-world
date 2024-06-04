958a3dfd25a4c04a907b08fab044a4c4ed0c0488
edwardnv-exd <edward_nv@exdion.com>
2024-06-04 16:07:28 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+# hello_world_package/hello.py
+
+class HelloWorld:
+    def say_hello(self):
+        return "Hello, world!"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "hello_world_package",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = [],
+    url = "https://github.com/edwardnv-exd/hello-world",
+    author = "Edward",
+    author_email = "edward_nv@exdion.dev",
+    description = "A simple hello world package"
+)
